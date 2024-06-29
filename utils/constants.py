b1b6a6e89c361fafc6f42bd103c43bd4a2857585
silentnoname <635397523pl@gmail.com>
2024-06-30 02:15:59 +0800

    add more models

--- utils/constants.py
+++ utils/constants.py
@@ -14,24 +14,33 @@ gemma_template = {
 
 model2template = {
     "Qwen/Qwen1.5-0.5B": qwen_template,
+    "Qwen/Qwen2-1.5B": qwen_template,
     "Qwen/Qwen1.5-1.8B": qwen_template,
+    "Qwen/Qwen1.5-4B": qwen_template,
     "Qwen/Qwen1.5-7B": qwen_template,
+    "Qwen/Qwen2-7B": qwen_template,
     "google/gemma-2b": gemma_template,
     "google/gemma-7b": gemma_template,
 }
 
 model2size = {
     "Qwen/Qwen1.5-0.5B": 620_000_000,
+    "Qwen/Qwen2-1.5B": 1_540_000_000,
     "Qwen/Qwen1.5-1.8B": 1_840_000_000,
+    "Qwen/Qwen1.5-4B": 3_950_000_000,
     "Qwen/Qwen1.5-7B": 7_720_000_000,
+    "Qwen/Qwen2-7B": 7_620_000_000,
     "google/gemma-2b": 2_510_000_000,
     "google/gemma-7b": 8_540_000_000,
 }
 
 model2base_model = {
     "Qwen/Qwen1.5-0.5B": "qwen1.5",
+    "Qwen/Qwen2-1.5B": "qwen1.5",
     "Qwen/Qwen1.5-1.8B": "qwen1.5",
+    "Qwen/Qwen1.5-4B": "qwen1.5",
     "Qwen/Qwen1.5-7B": "qwen1.5",
+    "Qwen/Qwen2-7B": "qwen1.5",
     "google/gemma-2b": "gemma",
     "google/gemma-7b": "gemma",
 }
